e45322fcb8b79ff950b84e1629c1cbe5f7cabc7a
basu <basu@basu.com>
2024-05-05 19:57:30 -0700

    First commit

---
+++ src/config/connectDB.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+console.log("process.env.DATABASE_HOST",process.env.DATABASE_HOST);
+console.log("process.env.DATABASE_USER",process.env.DATABASE_USER);
+console.log("process.env.DATABASE_PASSWORD",process.env.DATABASE_PASSWORD);
+console.log("process.env.DATABASE_NAME",process.env.DATABASE_NAME);
+const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+});
+
+export default connection;
